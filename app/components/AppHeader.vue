b3523d723a6af453ae1db30c98a4e6d2a8f4b34e
Rajeev R Sharma <i.rarsh@gmail.com>
2024-11-02 15:45:07 +0530

    feat: create basic app header

---
+++ app/components/AppHeader.vue
@@ -0,0 +1,11 @@
+<template>
+  <header
+    class="flex items-center h-14 px-4 bg-gray-50/75 dark:bg-gray-950/75 backdrop-blur border-b -mb-px sticky top-0 z-50 border-gray-200 dark:border-gray-800"
+  >
+    <h1 class="text-xl md:text-2xl">{{ title }}</h1>
+  </header>
+</template>
+
+<script setup lang="ts">
+defineProps<{ title: string }>();
+</script>
