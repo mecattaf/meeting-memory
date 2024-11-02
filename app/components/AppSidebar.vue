7bba54d6b9e701217ce80ae2dabefa7d9aa2c8b4
Rajeev R Sharma <i.rarsh@gmail.com>
2024-11-02 15:45:25 +0530

    feat: create basic app sidebar

---
+++ app/components/AppSidebar.vue
@@ -0,0 +1,17 @@
+<template>
+  <div class="h-screen w-64 border-r border-gray-200 dark:border-gray-800">
+    <div
+      class="flex items-center px-4 h-14 border-b border-gray-200 dark:border-gray-800"
+    >
+      <ULink class="font-bold text-2xl md:text-3xl px-2.5" to="/">
+        V<span class="text-primary">Notes</span>
+      </ULink>
+    </div>
+
+    <UVerticalNavigation class="p-4" :links="links" />
+  </div>
+</template>
+
+<script setup lang="ts">
+defineProps<{ links: { label: string; icon: string; to: string }[] }>();
+</script>
