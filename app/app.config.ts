6e5795e73374955e9835c258519a64acbdd33333
Rajeev R Sharma <i.rarsh@gmail.com>
2024-11-04 23:36:06 +0530

    chore: define app ui config

---
+++ app/app.config.ts
@@ -0,0 +1,11 @@
+export default defineAppConfig({
+  ui: {
+    primary: 'sky',
+    gray: 'slate',
+    card: {
+      header: {
+        base: 'flex flex-wrap items-center justify-between',
+      },
+    },
+  },
+});
